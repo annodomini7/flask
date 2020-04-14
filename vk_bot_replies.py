93c174e38228ad3eff2b1fd3bba44d6e73f8b9d5
ArtMGreen <artyom5145@gmail.com>
2020-04-14 13:30:26 +0300

    - Added new method: ask_city() in class VkBotReplies, it is called when bot's keyboard is unavailable or by user's choice - Added comments to all the methods (in Russian)

--- vk_bot_replies.py
+++ vk_bot_replies.py
@@ -12,13 +12,13 @@ class VkBotReplies:
         self.vk = self.vk_session.get_api()
         self.longpoll = VkBotLongPoll(self.vk_session, bot_id)
 
-    def start(self, send_to):
+    def start(self, send_to):    # Первое сообщение, приветствие и вопрос о лекарстве
         username = self.vk.users.get(user_ids=send_to)['response']['first_name']
         self.vk.messages.send(peer_id=send_to,
                               random_id=random.randint(0, 2 ** 64),
                               message=f'Здравствуйте, {username}! Какой препарат вам необходимо найти?')
 
-    def ask_for_location(self, send_to):
+    def ask_for_location(self, send_to):    # Запрос местоположения или запрос города - зависит от выбора пользователя
         keyboard = '''{
             "one_time": true,
             "buttons": [
@@ -45,3 +45,9 @@ class VkBotReplies:
                               message='Укажите свой город или отправьте местоположение, '
                                       'чтобы я нашёл ближайшие аптеки.',
                               keyboard=keyboard)
+
+    def ask_city(self, send_to):    # Вызывается при недоступности клавиатуры бота или по выбору пользователя
+        self.vk.messages.send(peer_id=send_to,
+                              random_id=random.randint(0, 2 ** 64),
+                              message='Укажите свой город, '
+                                      'чтобы я нашёл ближайшие аптеки.')

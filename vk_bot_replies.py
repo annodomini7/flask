8a63c9b8fbcb6867165999bf083429be611fcc8c
ArtMGreen <artyom5145@gmail.com>
2020-04-19 15:36:33 +0300

    - Added new method: create_keyboard() It allows to construct new bot's keyboards in the code

--- vk_bot_replies.py
+++ vk_bot_replies.py
@@ -1,16 +1,25 @@
-import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll
 import random
 
 
 class VkBotReplies:
     """Этот класс содержит методы, отвечающие за, как иронично, ответы бота."""
-    def __init__(self, bot_token, bot_id):
-        self.id = bot_id
-        self.token = bot_token
-        self.vk_session = vk_api.VkApi(token=bot_token)
-        self.vk = self.vk_session.get_api()
-        self.longpoll = VkBotLongPoll(self.vk_session, bot_id)
+    def __init__(self, vk_session):    # служебный метод
+        self.vk = vk_session.get_api()
+
+    def create_keyboard(self, *labels, one_time=True):    # служебный метод формирования клавиатуры
+        keyboard = {'one_time': one_time,
+                    'buttons': [[] for x in range(len(labels))]}
+        for label in enumerate(labels):
+            payload = r'{\"button\": \"' + str(label[0] + 1) + r'\"}'
+            if label[1] == 'SEND_LOCATION':
+                keyboard['buttons'][label[0]].append({'action': {'type': 'location',
+                                                                 'payload': payload}})
+            else:
+                keyboard['buttons'][label[0]].append({'action': {'type': 'text',
+                                                                 'payload': payload,
+                                                                 'label': label[1]},
+                                                      'color': 'primary'})
+        return str(keyboard).replace("'", '"').replace('True', 'true').replace('False', 'false')
 
     def start(self, send_to):    # Первое сообщение, приветствие и вопрос о лекарстве
         username = self.vk.users.get(user_ids=send_to)['response']['first_name']
@@ -19,27 +28,7 @@ class VkBotReplies:
                               message=f'Здравствуйте, {username}! Какой препарат вам необходимо найти?')
 
     def ask_for_location(self, send_to):    # Запрос местоположения или запрос города - зависит от выбора пользователя
-        keyboard = '''{
-            "one_time": true,
-            "buttons": [
-                [{
-                    "action": {
-                        "type": "location",
-                        "payload": "{\\"button\\": \\"1\\"}"
-                    }
-                }],
-                [
-                    {
-                        "action": {
-                            "type": "text",
-                            "payload": "{\\"button\\": \\"2\\"}",
-                            "label": "Указать город"
-                        },
-                        "color": "primary"
-                    }
-                ]
-            ]
-        }'''
+        keyboard = self.create_keyboard('SEND_LOCATION', 'Указать город')
         self.vk.messages.send(peer_id=send_to,
                               random_id=random.randint(0, 2 ** 64),
                               message='Укажите свой город или отправьте местоположение, '

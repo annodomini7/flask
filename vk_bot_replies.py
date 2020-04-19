b43a5a3aaf4ed640c10132c8ae18f49571fa5782
ArtMGreen <artyom5145@gmail.com>
2020-04-19 23:47:26 +0300

    - Replaced create_keyboard() method with keyboard creating instruments in vk_api library

--- vk_bot_replies.py
+++ vk_bot_replies.py
@@ -1,25 +1,17 @@
 import random
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 
 class VkBotReplies:
     """Этот класс содержит методы, отвечающие за, как иронично, ответы бота."""
-    def __init__(self, vk_session):    # служебный метод
+    def __init__(self, vk_session):    # служебный метод: инициализация экземпляра класса + создание клавиатур
         self.vk = vk_session.get_api()
 
-    def create_keyboard(self, *labels, one_time=True):    # служебный метод формирования клавиатуры
-        keyboard = {'one_time': one_time,
-                    'buttons': [[] for x in range(len(labels))]}
-        for label in enumerate(labels):
-            payload = r'{\"button\": \"' + str(label[0] + 1) + r'\"}'
-            if label[1] == 'SEND_LOCATION':
-                keyboard['buttons'][label[0]].append({'action': {'type': 'location',
-                                                                 'payload': payload}})
-            else:
-                keyboard['buttons'][label[0]].append({'action': {'type': 'text',
-                                                                 'payload': payload,
-                                                                 'label': label[1]},
-                                                      'color': 'primary'})
-        return str(keyboard).replace("'", '"').replace('True', 'true').replace('False', 'false')
+        self.location_keyboard = VkKeyboard(one_time=True)
+        self.location_keyboard.add_line()
+        self.location_keyboard.add_location_button(payload='location')
+        self.location_keyboard.add_line()
+        self.location_keyboard.add_button('Указать город', payload='wait_for_city')
 
     def start(self, send_to):    # Первое сообщение, приветствие и вопрос о лекарстве
         username = self.vk.users.get(user_ids=send_to)['response']['first_name']
@@ -28,7 +20,7 @@ class VkBotReplies:
                               message=f'Здравствуйте, {username}! Какой препарат вам необходимо найти?')
 
     def ask_for_location(self, send_to):    # Запрос местоположения или запрос города - зависит от выбора пользователя
-        keyboard = self.create_keyboard('SEND_LOCATION', 'Указать город')
+        keyboard = self.location_keyboard.get_keyboard()
         self.vk.messages.send(peer_id=send_to,
                               random_id=random.randint(0, 2 ** 64),
                               message='Укажите свой город или отправьте местоположение, '
